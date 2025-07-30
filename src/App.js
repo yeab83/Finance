4ef87b9b37b510c1b2c650d18c181098403b1130
yeab83 <yeabsiraabera83@gmail.com>
2025-07-30 17:19:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react'
+import Navbar from './component/navbar';
+import Hero from './component/Hero';
+import Analyics from './component/Analyics';
+import Newsletter from './component/Newsletter';
+import Cards from './component/cards';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <Navbar/>
+    <Hero/>
+    <Analyics/>
+    <Newsletter/>
+    <Cards/>
+    <Footer/>
+  </div>
+  )
 }
 
 export default App;
